e303fb132fb2c9c6b4bc2939feade4e574de2b6e
Lancelot Edwards <lancelot.edwards@gmail.com>
2017-08-09 14:09:15 -0500

    add custom label to text fields

--- window-shopper/View/CurrencyTextField.swift
+++ window-shopper/View/CurrencyTextField.swift
@@ -11,6 +11,23 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
   
+  override func draw(_ rect: CGRect) {
+    let size: CGFloat = 20
+    let labelFrame = CGRect(x: 5, y: (frame.size.height/2)-size/2, width: size, height: size)
+    let currencyLabel = UILabel(frame: labelFrame)
+    currencyLabel.backgroundColor = #colorLiteral(red: 0.7761658269, green: 0.7761658269, blue: 0.7761658269, alpha: 0.8)
+    currencyLabel.textAlignment = .center
+    currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+    currencyLabel.layer.cornerRadius = 5.0
+    currencyLabel.clipsToBounds = true
+    
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currency
+    formatter.locale = .current
+    currencyLabel.text = formatter.currencySymbol
+    addSubview(currencyLabel)
+  }
+  
   override func prepareForInterfaceBuilder() {
     customizeView()
   }
@@ -24,6 +41,7 @@ class CurrencyTextField: UITextField {
     backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
     layer.cornerRadius = 5.0
     textAlignment = .center
+    clipsToBounds = true
     
     if let placeholder = placeholder {
       let place = NSAttributedString(string: placeholder, attributes: [.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
